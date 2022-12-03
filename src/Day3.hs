80999c849d57df448d0f56ef8036d179a0974b28
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-03 15:37:00 +0000

    cleaned up day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -3,7 +3,7 @@ module Day3
   )
 where
 
-import Common
+import Common (count, get3rd, indexOf, takeInN)
 import Data.Char (chr, isAsciiLower, ord)
 import Data.List (minimumBy, sortBy)
 
@@ -11,11 +11,6 @@ toAsciiScheme c = do
   if isAsciiLower c
     then ord c - 96
     else ord c - 38
-
-fromAsciiScheme c = do
-  if c < 26
-    then chr (c + 96)
-    else chr (c + 38)
 
 readRucksacksInNGroups :: String -> Int -> IO [[String]]
 readRucksacksInNGroups filename n = do
@@ -25,19 +20,19 @@ readRucksacksInNGroups filename n = do
 
 commonItemType ruckSacksList = do
   let midPoint lst = length lst `div` 2
-  let duplicateList lst = get3rd (head (filter (\(lindex, uindex, _) -> lindex < midPoint lst && uindex > midPoint lst) [(indexOf c lst, length lst - indexOf c (reverse lst), c) | (i, c) <- zip [0 ..] lst, count (== c) lst > 1]))
-  sum [toAsciiScheme (duplicateList lst) | lst <- ruckSacksList]
+  let withinBound lb ub p = lb < p && ub > p
+  let commonItems lst = get3rd (head (filter (\(lb, ub, _) -> withinBound lb ub (midPoint lst)) [(indexOf c lst, length lst - indexOf c (reverse lst), c) | (i, c) <- zip [0 ..] lst, count (== c) lst > 1]))
+  sum [toAsciiScheme (commonItems lst) | lst <- ruckSacksList]
 
 findCommonBadge firstRuckSack secondRuckSck thirdRuckSack = do
   head [char | char <- firstRuckSack, char `elem` secondRuckSck, char `elem` thirdRuckSack]
 
 commonBadgeType ruckSacksList = do
-  let nthRuckSack lst n = lst !! n
-  sum [toAsciiScheme (findCommonBadge (nthRuckSack lst 0) (nthRuckSack lst 1) (nthRuckSack lst 2)) | lst <- ruckSacksList]
+  sum [toAsciiScheme (findCommonBadge (head lst) (lst !! 1) (lst !! 2)) | lst <- ruckSacksList]
 
 day3 = do
   print "***Day 3***"
-  ruckSacksList <- readRucksacksInNGroups "./inputs/input3.demo" 1
+  ruckSacksList <- readRucksacksInNGroups "./inputs/input3.txt" 1
   print (commonItemType (concat ruckSacksList))
   ruckSacksList <- readRucksacksInNGroups "./inputs/input3.txt" 3
   print (commonBadgeType ruckSacksList)
