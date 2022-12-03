b72d257f715e438912d445dedaafd685067b6824
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-03 15:42:29 +0000

    added type signatures for day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -7,7 +7,7 @@ import Common (count, get3rd, indexOf, takeInN)
 import Data.Char (chr, isAsciiLower, ord)
 import Data.List (minimumBy, sortBy)
 
-toAsciiScheme c = do
+toPriority c = do
   if isAsciiLower c
     then ord c - 96
     else ord c - 38
@@ -18,21 +18,24 @@ readRucksacksInNGroups filename n = do
   let newLns = concatMap (takeInN n) [lns]
   return newLns
 
-commonItemType ruckSacksList = do
+sumOfItemPriorities :: [[Char]] -> Int
+sumOfItemPriorities ruckSacksList = do
   let midPoint lst = length lst `div` 2
   let withinBound lb ub p = lb < p && ub > p
   let commonItems lst = get3rd (head (filter (\(lb, ub, _) -> withinBound lb ub (midPoint lst)) [(indexOf c lst, length lst - indexOf c (reverse lst), c) | (i, c) <- zip [0 ..] lst, count (== c) lst > 1]))
-  sum [toAsciiScheme (commonItems lst) | lst <- ruckSacksList]
+  sum [toPriority (commonItems lst) | lst <- ruckSacksList]
 
-findCommonBadge firstRuckSack secondRuckSck thirdRuckSack = do
-  head [char | char <- firstRuckSack, char `elem` secondRuckSck, char `elem` thirdRuckSack]
+findCommonBadge :: String -> String -> String -> Char
+findCommonBadge firstRuckSack secondRuckSack thirdRuckSack = do
+  head [char | char <- firstRuckSack, char `elem` secondRuckSack, char `elem` thirdRuckSack]
 
+commonBadgeType :: [[String]] -> Int
 commonBadgeType ruckSacksList = do
-  sum [toAsciiScheme (findCommonBadge (head lst) (lst !! 1) (lst !! 2)) | lst <- ruckSacksList]
+  sum [toPriority (findCommonBadge (head lst) (lst !! 1) (lst !! 2)) | lst <- ruckSacksList]
 
 day3 = do
   print "***Day 3***"
   ruckSacksList <- readRucksacksInNGroups "./inputs/input3.txt" 1
-  print (commonItemType (concat ruckSacksList))
+  print (sumOfItemPriorities (concat ruckSacksList))
   ruckSacksList <- readRucksacksInNGroups "./inputs/input3.txt" 3
   print (commonBadgeType ruckSacksList)
