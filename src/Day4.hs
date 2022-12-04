bf28807a21abc50200888b2083e368686ddf7d54
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-04 13:13:21 +0000

    cleaned up day4!

--- src/Day4.hs
+++ src/Day4.hs
@@ -10,20 +10,12 @@ readSectionAssignments filename = do
   lns <- lines <$> readFile filename
   return $ map (map (\x -> read x :: Int) . splitOn '-') <$> map (splitOn ',') lns
 
-totalCompletelyOverlappingPairs :: [[[Int]]] -> Int
-totalCompletelyOverlappingPairs sections = do
-  count (/= []) [checkSubset firstS secondS | [firstS, secondS] <- sections]
-  where
-    checkSubset firstS secondS = [firstS | subset [head firstS .. last firstS] [head secondS .. last secondS] || subset [head secondS .. last secondS] [head firstS .. last firstS]]
-
-totalOverlappingPairs :: [[[Int]]] -> Int
-totalOverlappingPairs sections = do
-  count (/= []) [checIntersection firstS secondS | [firstS, secondS] <- sections]
-  where
-    checIntersection firstS secondS = [firstS | [head firstS .. last firstS] `intersect` [head secondS .. last secondS]]
+totalPairs :: [[[Int]]] -> ([Int] -> [Int] -> Bool) -> Int
+totalPairs sections fn = do
+  count (== True) [fn [head f .. last f] [head s .. last s] | [f, s] <- sections]
 
 day4 = do
   print "***Day 4***"
   sections <- readSectionAssignments "./inputs/input4.txt"
-  print (totalCompletelyOverlappingPairs sections)
-  print (totalOverlappingPairs sections)
+  print (totalPairs sections (\x y -> subset x y || subset y x))
+  print (totalPairs sections intersect)
