d283b3bfd863cb3598072b2f138413b402bf7115
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-05 21:57:06 +0000

    day5 almost works

--- src/Day5.hs
+++ src/Day5.hs
@@ -5,8 +5,9 @@ module Day5
 where
 
 import Common (indexOf, replace, splitOn)
+import Control.Monad
 
--- readStackAndProcedures :: String -> IO [[[Int]]]
+readStackAndProcedures :: String -> IO ([[String]], [[Int]])
 readStackAndProcedures filename = do
   lns <- lines <$> readFile filename
   let [stacksLns, procedures] = splitOn "" lns
@@ -20,7 +21,26 @@ readStackAndProcedures filename = do
     totalSpaces n = take n $ cycle " "
     removeWords word = word `notElem` ["move", "from", "to"]
 
+printStack stacks = do
+  sequence_ [putStr (el1 ++ "\t" ++ el2 ++ "\t" ++ el3 ++ "\n") | (el1, el2, el3) <- zip3 (head stacks) (stacks !! 1) (stacks !! 2)]
+
+applyProcedure :: [Int] -> [[String]] -> [[String]]
+applyProcedure procedure stacks = do
+  let cleanStack = [removeFromStack stack stackIdx | (stackIdx, stack) <- zip [1 ..] stacks]
+  [addToStack stack stackIdx | (stackIdx, stack) <- zip [1 ..] cleanStack]
+  where
+    [totalElementsToRemove, from, to] = procedure
+    elementsToAdd = concat [take totalElementsToRemove stack | (stackIdx, stack) <- zip [1 ..] stacks, stackIdx == from]
+    removeFromStack stack stackIdx
+      | stackIdx == from = drop totalElementsToRemove stack
+      | otherwise = stack
+    addToStack stack stackIdx
+      | stackIdx == to = reverse elementsToAdd ++ filter (/= "") stack
+      | otherwise = stack
+
 day5 = do
   print "***Day 5***"
-  stacks <- readStackAndProcedures "./inputs/input5.demo"
-  print stacks
+  (stacks, procedures) <- readStackAndProcedures "./inputs/input5.demo"
+  print (foldr applyProcedure stacks (reverse procedures))
+
+-- print (filter (\x -> x `notElem` ['[', ']']) (concatMap head (foldr applyProcedure stacks (reverse procedures))))
