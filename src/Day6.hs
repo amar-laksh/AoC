e66510f1b91344134c41237a5c6beee057362965
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-06 18:08:03 +0000

    day6 finished!

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,35 +1,25 @@
-{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
-
 module Day6
   ( day6,
   )
 where
 
-import Common (indexOf, isSet, takeInN, zipAll)
-import Data.List (zip4)
-import Data.Maybe (fromMaybe, listToMaybe)
+import Common (isSet, zipAll)
 
 readDataStream :: String -> IO [String]
 readDataStream filename = lines <$> readFile filename
 
-uniqueMarker [] = False
-uniqueMarker [c1, c2, c3, c4, c5, c6, c7]
-  | isSet [c1, c2, c3, c4, c5, c6, c7] = True
-  | otherwise = False
-uniqueMarker [c1, c2, c3, c4]
-  | isSet [c1, c2, c3, c4] = True
-  | otherwise = False
-uniqueMarker elements = False
-
-detect4CharMarker :: String -> Int
-detect4CharMarker dataStream = head [last (map fst lst) + 1 | lst <- zipAll [pairs 0, pairs 1, pairs 2, pairs 3], uniqueMarker (map snd lst)]
+detectMarker :: Int -> String -> Int
+detectMarker markerWidth dataStream = head [last (map fst tuples) + 1 | tuples <- zipAll markersList, isSet (map snd tuples)]
   where
-    pairs n = zip [n ..] (drop n dataStream)
+    markersList = [markerTuple n | n <- [0 .. markerWidth - 1]]
+    markerTuple n = zip [n ..] (drop n dataStream)
 
 day6 = do
   print "***Day 6***"
   dataStreams <- readDataStream "./inputs/input6.demo"
-  print (map detect4CharMarker dataStreams == [7, 5, 6, 10, 11])
+  print (map (detectMarker 4) dataStreams)
+  print (map (detectMarker 14) dataStreams)
 
   dataStreams <- readDataStream "./inputs/input6.txt"
-  print (map detect4CharMarker dataStreams == [1623])
+  print (map (detectMarker 4) dataStreams)
+  print (map (detectMarker 14) dataStreams)
