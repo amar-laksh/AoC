68c2fb8ceff2f613ddb18acd16487af7c09dd96e
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-06 20:00:04 +0000

    added benchmark suite

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aoc
+
+This contains the solutions to AoC 2022 in haskell and the bench suite:
+
+!(benchMarks)[./bench.svg]
--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,20 +1,21 @@
-import Day1 as T1
+import qualified Day1 as D1
+import qualified Day2 as D2
+import qualified Day3 as D3
+import qualified Day4 as D4
+import qualified Day5 as D5
+import qualified Day6 as D6
 import Test.Tasty.Bench
-
-msgs :: [Char]
-msgs = ['a' .. 'z'] :: [Char]
 
 main :: IO ()
 main =
   defaultMain
     [ bgroup
-        "Benchmark (Pure)"
-        [ bcompare "a to z characters" $ bench "T1.helloWorldPure" $ nf T1.helloWorldPure msgs,
-          bcompare "a to z characters" $ bench "raw ++" $ nf ("hello world: " ++) msgs
-        ],
-      bgroup
-        "Benchmark (Monadic)"
-        [ bcompare "a to z characters" $ bench "T1.helloWorldMonadic" $ nfIO (T1.helloWorldMonadic msgs),
-          bcompare "a to z characters" $ bench "putStrLn" $ nfIO (putStrLn ("hello world: " ++ msgs))
+        "2022 "
+        [ bcompare "Days" $ bench "day1" $ nfIO D1.day1,
+          bench "day2" $ nfIO D2.day2,
+          bench "day3" $ nfIO D3.day3,
+          bench "day4" $ nfIO D4.day4,
+          bench "day5" $ nfIO D5.day5,
+          bench "day6" $ nfIO D6.day6
         ]
     ]
