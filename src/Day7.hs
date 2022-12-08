57fe14f575f75bebec0bd0be84349e3905918bb3
Amar Lakshya <amar.lakshya@protonmail.com>
2022-12-08 01:15:35 +0000

    WIP for now

--- src/Day7.hs
+++ src/Day7.hs
@@ -12,36 +12,52 @@ paramCmds = ["cd"]
 
 params = ["/", ".."]
 
-data Commands = ChangeDirectory | ListDirectories | InvalidCommand deriving (Show, Eq, Ord)
+listingTypes = ["dir"]
 
 data Directory = Directory String | Root | BackDirectory | InvalidDirectory deriving (Show, Eq, Ord)
 
-data Output o = DirectoryType Directory | Files String | InvalidOutput deriving (Show, Eq, Ord)
+data Commands = ChangeDirectory Directory | ListDirectories | InvalidCommand deriving (Show, Eq, Ord)
 
-getDirectory :: String -> Output String
+data Output = DirectoryType Directory | File String | FileWithSize (String, Int) | DirectoryWithSize (String, Int) | InvalidOutput deriving (Show, Eq, Ord)
+
+data Tree = PNode Output | CNode Output | Tree
+
+getDirectory :: String -> Directory
 getDirectory dir
-  | dir == head params = DirectoryType Root
-  | dir == last params = DirectoryType BackDirectory
-  | otherwise = DirectoryType (Directory dir)
+  | dir == head params = Root
+  | dir == last params = BackDirectory
+  | otherwise = Directory dir
 
-getListings :: String -> Output String
+getListings :: [String] -> [Output]
 getListings lst = do
-  Files lst
+  [parseListing l | l <- lst]
+  where
+    parseListing l
+      | head (words l) == head listingTypes = DirectoryType (getDirectory (last (words l)))
+      | '.' `notElem` last (words l) = DirectoryWithSize (last (words l), read (head (words l)) :: Int)
+      | otherwise = FileWithSize (last (words l), read (head (words l)) :: Int)
 
-parseOutput :: [String] -> (Commands, Output String)
+parseOutput :: [String] -> (Commands, [Output])
 parseOutput [paramCmd]
-  | head (words paramCmd) == head paramCmds = (ChangeDirectory, getDirectory (last (words paramCmd)))
+  | head (words paramCmd) == head paramCmds = (ChangeDirectory (getDirectory (last (words paramCmd))), [])
 parseOutput output
-  | head output == head cmds = (ListDirectories, getListings (concatMap (++ " ") (drop 1 output)))
-  | otherwise = (InvalidCommand, InvalidOutput)
+  | head output == head cmds = (ListDirectories, getListings (drop 1 output))
+  | otherwise = (InvalidCommand, [InvalidOutput])
 
-parseOutputs :: [[String]] -> [(Commands, Output String)]
+parseOutputs :: [[String]] -> [(Commands, [Output])]
 parseOutputs outputs = do
   [parseOutput output | output <- outputs]
+
+tree :: [(Commands, [Output])] -> [Commands]
+tree lst = do
+  [c | (c, o) <- lst, null o]
 
 day7 = do
   print "***Day 7***"
   terminalOutput <- readTerminal "./inputs/input7.demo"
   let inputOutputs = filter (/= []) (splitOn '$' terminalOutput)
   let tlst = map (filter (/= "") . splitOn '\n' . (\x -> if head x == ' ' then drop 1 x else x)) inputOutputs
-  print (parseOutputs tlst)
+  let ast = parseOutputs tlst
+  print (map tree [ast])
+
+-- print (map tree (parseOutputs tlst))
